2c2292611d9302fd67e3c67ad7bd64a75b2aab4b
Azfar Shams <azfarshams1998@gmail.com>
2024-02-02 03:51:22 +0600

    Update PlayerController.cs

--- Funny Jump Game/Assets/Scripts/PlayerController.cs
+++ Funny Jump Game/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,15 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 10f;
     private BoxCollider2D groundCheckCollider;
+    private SpriteRenderer spriteRenderer;
+    private Camera mainCamera;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         groundCheckCollider = GetComponent<BoxCollider2D>(); // Get the BoxCollider2D
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        mainCamera = Camera.main;
     }
 
     void Update()
@@ -25,6 +29,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            ChangeColors();
         }
     }
 
@@ -34,5 +39,31 @@ public class PlayerController : MonoBehaviour
         return Physics2D.OverlapBox(groundCheckCollider.bounds.center,
                                    groundCheckCollider.bounds.size,
                                    0f, LayerMask.GetMask("Ground"));
+    }
+
+    void ChangeColors()
+    {
+        Color newPlayerColor;
+        Color newBackgroundColor;
+
+        // Generate distinct random colors
+        do
+        {
+            newPlayerColor = Random.ColorHSV();
+            newBackgroundColor = Random.ColorHSV();
+        } while (CalculateColorDifference(newPlayerColor, newBackgroundColor) < 0.5f);
+
+        spriteRenderer.color = newPlayerColor;
+        mainCamera.backgroundColor = newBackgroundColor;
+    }
+
+    // Custom function to calculate color difference based on Euclidean distance in RGB space
+    float CalculateColorDifference(Color color1, Color color2)
+    {
+        float difference = 0f;
+        difference += Mathf.Pow(color1.r - color2.r, 2f);
+        difference += Mathf.Pow(color1.g - color2.g, 2f);
+        difference += Mathf.Pow(color1.b - color2.b, 2f);
+        return Mathf.Sqrt(difference);
     }
 }
