e372d3bed897661d52db456dc82663da50707e23
Azfar Shams <azfarshams1998@gmail.com>
2024-02-02 00:17:54 +0600

    Update PlayerController.cs

--- Funny Jump Game/Assets/Scripts/PlayerController.cs
+++ Funny Jump Game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private bool isGrounded;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Movement
+        float horizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Check for ground
+        isGrounded = Physics2D.OverlapCircle(transform.position, 0.4f, 
+                                            LayerMask.GetMask("Ground"));
     }
 }
